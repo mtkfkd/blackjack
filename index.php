54255eca2a4afbe3420514923851e7d5bb603c95
MITSUKI\mitsuki <fkdmtk5252@gmail.com>
2017-11-13 23:33:03 +0900

    スタート画面の追加

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>ブラックジャック</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <h1 class="title">BLACK JACK</h1>
+
+    <p class="start"><a href="play.php">Game Start</a></p>
+  </div> <!-- /.container -->
+</body>
+</html>
